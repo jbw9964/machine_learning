9b22deb820027dbb5623bc8aa1ccc48f4e9ca3e3
jbw9964 <jbw9964@gmail.com>
2022-08-10 22:43:58 +0900

    [2022.08.10] : 지도학습 - 경사하강법 공부 및 실습

--- practice/practice.py
+++ practice/practice.py
@@ -1,8 +1,26 @@
-print('\n')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-10, 10, 1)
+f_x = x ** 2
+
+print('hello world')
+
+x_new = 10
+derivative = []
+y = []
+
+learning_rate = 0.1
 
 
-print('hello')
+for i in range(100):
+    old_value = x_new
+    derivative.append(old_value - learning_rate * 2 * old_value)
 
+    x_new = old_value - learning_rate * 2 * old_value
+    y.append(x_new ** 2)
 
-
-print('\n')
+plt.plot(x, f_x)
+plt.scatter(derivative,y)
+plt.show()
